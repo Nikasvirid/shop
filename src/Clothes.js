3969bfe4fba3b38788a6920a8d2f7a23d92013f8
Svirid Veronika <sviridka-vp@mail.ru>
2023-02-09 10:46:49 +0300

    change name

--- src/Clothes.js
+++ src/Clothes.js
@@ -2,7 +2,7 @@ function Clothes({Dresses}){
     return(
         <div className="products">
             {Dresses.map((element => {
-                const{id, name, searchTerm, price, image} = element;
+                const{id, name, price, image} = element;
                 return(
                     <div className="product-card" key={id}>
                         <img src={image} width="400px"height="500px" alt="retail" />
